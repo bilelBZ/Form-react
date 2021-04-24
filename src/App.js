c5edb22d1d3c7e158521849069c9f4d0b453bcf5
bilelBZ <bilelbz155@gmail.com>
2021-04-24 15:54:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div>
+      <Form/>
+    </div>
+  )
+}
+
+export default App
